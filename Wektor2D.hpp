a8baa3847cdcb42fec53c564a1e71626fec15c0c
xardis321 <01161660@pw.edu.pl>
2023-10-29 20:38:14 +0100

    test1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,36 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+            double x;
+            double y;
+
+    public:
+        Wektor2D() { x=0.;  y=0.;}
+
+        Wektor2D(double coordX, double coordY) { 
+            x=coordX; 
+            y=coordY; 
+            }
+
+            void setX(double coordX) { x=coordX; }
+
+            double getX() { return x; }
+
+            void setY(double coordY) { y=coordY; }
+
+            double getY() { return y; }
+
+            Wektor2D operator+(Wektor2D vector){
+
+                Wektor2D v{x+vector.getX(), y+vector.getY() };
+                return v;
+
+            }
+
+            double operator*(Wektor2D vectorr){
+
+            return x*vectorr.getX()+y*vectorr.getY();
+
+            }
+
 };
